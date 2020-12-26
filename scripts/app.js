37201c6055d77fbf4aba999aec3bf6b85f518523
Keith <kg.anderson@outlook.com>
2020-12-26 19:17:03 +1300

    Create ship filter function

--- scripts/app.js
+++ scripts/app.js
@@ -1,29 +1,54 @@
-let shipData = document.querySelector('.jcr');
-let positionButton = document.querySelector('.grid-ship1')
-let heading1 = document.querySelector('.grid-header1')
+let map = document.querySelector('.map');
+let heading = document.querySelector("#heading");
+let jcr = document.querySelector('.grid-ship1 > .btn');
+let discovery = document.querySelector('.grid-ship2 > .btn');
+let jamesCook = document.querySelector('.grid-ship3 > .btn');
+let apiText = document.querySelector('.update-text');
 
 
-//JCR data is currently always [0] but will to update this so vessel specific can be selected and filtered out. Unless vessel specific API works in the end.
-let displayShipData = async () =>{ 
+let getShipData = async (callSign) => {
     try {
-    const currentLocation = await getShipData()
-    let latitude = currentLocation[0].latest_position.latitude;
-    let longitude = currentLocation[0].latest_position.longitude;
-    shipData.classList.add('map')
-    heading1.innerText = ""
-    shipData.innerHTML =
-`<iframe width="100%" height="240" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=${latitude},${longitude}&amp;q=${latitude},${longitude}&amp;ie=UTF8&amp;t=&amp;z=6&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>`
+        const allShipData = await getAllShipData()
+        let filteredVessel = shipFilter(callSign, allShipData)
+        let latitude = filteredVessel.latest_position.latitude;
+        let longitude = filteredVessel.latest_position.longitude;
+        let vesselName = filteredVessel.callsign;
+        map.classList.add('map')
+        heading.remove()
+        map.innerHTML =
+            `<iframe width="500px" height="240" src="https://maps.google.com/maps?width=100%&amp;height=600&amp;hl=en&amp;coord=${latitude},${longitude}&amp;q=${latitude},${longitude}&amp;ie=UTF8&amp;t=&amp;z=6&amp;iwloc=B&amp;output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe>`
+        displayText(vesselName)
     } catch {
-        heading1.innerText = "API call failed"
+        heading.innerText = "API call failed"
     }
 }
 
 //Endpoint does not work on ship level so have to return all data for all ships
-let getShipData = async () => {
-    const config = {headers: {'Accept':'application/json','Content-Type': 'application/json'}}
+let getAllShipData = async () => {
+    const config = { headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' } }
     let result = await axios.get("http://api.bas.ac.uk/marine/v1/vessels/position/", config)
     return result.data.data
 }
 
-positionButton.addEventListener('click', displayShipData)
+function shipFilter(callSign, allShipData) {
+    for (vessel of allShipData) {
+        if (vessel.callsign == callSign) {
+            return vessel
+        }
+    }
+}
 
+function displayText(vesselName) {
+    if(vesselName != "ZDLP") {
+        apiText.classList.remove("hidden")
+    }
+    else {
+        apiText.classList.add("hidden")
+    }
+}
+
+
+
+discovery.addEventListener('click', () => { getShipData("2FGX5") })
+jamesCook.addEventListener('click', () => { getShipData("MLRM6") })
+jcr.addEventListener('click', () => { getShipData("ZDLP") })
