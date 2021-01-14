6100ec11caa91b635eeda1de1087ce27a8ddc95a
Keith <kg.anderson@outlook.com>
2021-01-14 18:18:36 +1300

    Add server file

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.use(express.static("public"));
+
+app.listen(port, function () {
+  console.log('Server listening on port:', port)
+})
+
+
+
